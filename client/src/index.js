b9283c44785317f30ae54ebcbe1aa00496cf9d9b
NGUD-0119 <finaldev.dream.119@gmail.com>
2022-10-01 19:16:08 -0400

    updated index.js to wrap Provider around App and pass store as prop

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import store and Provider to App
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* make the store available to React components by wrapping Provider around App and passing store as prop */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
