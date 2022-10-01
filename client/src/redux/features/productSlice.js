a683c4f8f72c901a7ddea1b12e08c2a58438d955
NGUD-0119 <finaldev.dream.119@gmail.com>
2022-10-01 19:52:26 -0400

    added redux slice for product

---
+++ client/src/redux/features/productSlice.js
@@ -0,0 +1,22 @@
+// import createSlice API from Redux Toolkit to create Redux State Slice
+import { createSlice } from '@reduxjs/toolkit';
+
+// slice requires name, initial state, and reducers
+export const productSlice = createSlice({
+  name: 'product',
+  initialState: {
+    products: []
+  },
+  reducers: {
+    UPDATE_PRODUCTS: (state, action) => {
+      return {
+        ...state,
+        products: [...action.products]
+      };
+    }
+  }
+});
+
+export const { UPDATE_PRODUCTS } = productSlice.actions;
+
+export default productSlice.reducer
