45fdcc9fb63f73992d38ee3c5cbfb0fab058467e
NGUD-0119 <finaldev.dream.119@gmail.com>
2022-10-01 19:59:08 -0400

    added slice reducers to store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,16 @@
 // import configureStore API from Redux Toolkit
 import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './features/cartSlice';
+import productReducer from './features/productSlice';
+import categoryReducer from './features/categorySlice';
 
 // create Redux store with reducer
 const store = configureStore({
-  reducer: {}
+  reducer: {
+    cart: cartReducer,
+    product: productReducer,
+    category: categoryReducer
+  }
 })
 
 // export Redux store
