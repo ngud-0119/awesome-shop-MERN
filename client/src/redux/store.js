40b19fae2c4a7935bc355cd864bf6bb2bf943702
NGUD-0119 <finaldev.dream.119@gmail.com>
2022-10-01 19:08:27 -0400

    added redux store

---
+++ client/src/redux/store.js
@@ -0,0 +1,10 @@
+// import configureStore API from Redux Toolkit
+import { configureStore } from '@reduxjs/toolkit';
+
+// create Redux store with reducer
+const store = configureStore({
+  reducer: {}
+})
+
+// export Redux store
+export default store;
