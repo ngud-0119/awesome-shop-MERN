60029dbccc57e05879d9a47e6ee163c211758995
NGUD-0119 <finaldev.dream.119@gmail.com>
2022-10-01 20:00:19 -0400

    added comments

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,13 @@
 // import configureStore API from Redux Toolkit
 import { configureStore } from '@reduxjs/toolkit';
+// import slice reducers
 import cartReducer from './features/cartSlice';
 import productReducer from './features/productSlice';
 import categoryReducer from './features/categorySlice';
 
 // create Redux store with reducer
 const store = configureStore({
+  // use slice reducers to handle updates to state
   reducer: {
     cart: cartReducer,
     product: productReducer,
